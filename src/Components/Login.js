e074c5c87c2273e9439e08e961f8389b436a7c98
Cristian Espinoza <cristianantonioespinoza@hotmail.com>
2019-08-24 14:52:26 -0500

    Componentes Login, Admin y Categorys

---
+++ src/Components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'antd';
+import { Button } from 'antd';
+
+export default class login extends Component {
+    render() {
+        return (
+            <div >                    
+            </div>
+        )
+    }
+}
+
