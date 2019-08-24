0075ba18b3cb3a855ca84e8af719fb141401c32b
Armando Rosas <armando.rosas.mkt@gmail.com>
2019-08-24 14:56:18 -0500

    Componente Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import BannerFullwidth from '../assets/img/BannerFullwidth.png'
+import { Typography, Divider } from 'antd';
+
+
+export default class Banner extends Component {
+    render() {
+        return (
+            <div>
+                 <Row>
+                    <Col span={24}> <div className='banner'><h1 className='bannerText'>
+                        Gana dinero rentando 
+                        <br/>lo que no necesitas</h1> </div></Col>
+                </Row>
+            </div>
+        )
+    }
+}
