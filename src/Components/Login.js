301c7e0dbed3c56acd26281a6bd885eb1e7788db
Cristian Espinoza <cristianantonioespinoza@hotmail.com>
2019-08-31 12:14:13 -0500

    Login con modal

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd';
 import { Button } from 'antd';
+import { Modal } from 'antd';
+
 
 export default class login extends Component {
+    state = { visible: false };
+
+    showModal = () => {
+        this.setState({
+        visible: true,
+        });
+    };
+
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+        visible: false,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+        visible: false,
+        });
+    };
+
     render() {
         return (
-            <div >                    
+            <div >
+                <Button type="primary" onClick={this.showModal} >
+                    Login
+                </Button>
+                <Modal
+                    title=" Login "
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                </Modal>
             </div>
-        )
+        );
     }
-}
+    }
+
 
