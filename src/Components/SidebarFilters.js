101fe5698060902231b06c289faed73addebc920
Luis Fernando Martinez <luis.mtz.code@gmail.com>
2019-09-06 07:02:10 -0500

    Select

--- src/Components/SidebarFilters.js
+++ src/Components/SidebarFilters.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Layout, List, Typography } from 'antd';
+import { Layout, List, Typography, Breadcrumb, Select } from 'antd';
 
+const { Option } = Select;
 const { Sider } = Layout;
 
 
 export default class SidebarFilters extends Component {
     componentWillMount() {
         this.data = [
-            <h1>Racing car sprays burning fuel into crowd.</h1>,
+            <Select name="order" id="order" defaultValue="relevant" onChange={this.handleChange}>
+                <Option value="relevant">Más relevantes</Option>
+                <Option value="low-price">Menor precio</Option>
+                <Option value="hight-price">Mayor precio</Option>
+            </Select>,
             'Japanese princess to wed commoner.',
             'Australian walks 100km after outback crash.',
             'Man charged over missing wedding girl.',
@@ -16,17 +21,25 @@ export default class SidebarFilters extends Component {
         ];
     }
 
+    handleChange(){
+
+    }
+
     render() {
         return (
             <Sider width={200} className="transparent sideBarFilter">
                 <List
-                    header={<div>Header</div>}
+                    header={
+                     <Breadcrumb>
+                        <Breadcrumb.Item>Productos</Breadcrumb.Item>
+                     </Breadcrumb>
+                    }
                     footer={<div>Footer</div>}
                     bordered
                     dataSource={this.data}
                     renderItem={item => (
                         <List.Item>
-                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                            {item}
                         </List.Item>
                     )}
                 />
