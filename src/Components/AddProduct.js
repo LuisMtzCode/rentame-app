ef6baf680ceb5d319e4c3e41eedf9b4200526db9
abrahan romo ramos <abrahan.romo@gmail.com>
2019-09-07 12:03:37 -0500

    cambios hechos en Trello, me falta la función de guardar, sigue eso

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -4,6 +4,7 @@ import styles from '../App.css';
 import { Form, Input, DatePicker, TimePicker, Select, Cascader, InputNumber, Button } from 'antd';
 
 const { Option } = Select;
+const { TextArea } = Input;
 
 const formItemLayout = {
   labelCol: {
@@ -26,73 +27,110 @@ export default class AddProduct extends Component {
         ];
     }
 
+    state = {
+        value: '',
+      };
+
+      onChange = ({ target: { value } }) => {
+        this.setState({ value });
+      };
+
     render() {
+
+        const { value } = this.state;
+
         return (
             <div>  
 
                 {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNLRFAFPwxrcmp9ecUoR5G-xnezjE2SLZG1rF4iinM5MPONJzb" class="imagen"/> */}
 
-                <p>Ingrese los siguientes datos para dar de alta un artículo:</p>
+                
+                    <p>Ingrese los siguientes datos para dar de alta un artículo:</p>
+                    
+               
                 {/* <center>This text will be center-aligned.</center> */}
 
                 <Form {...formItemLayout}>
+                    <div style={{ margin: '12px 0' }} />
                     <Form.Item
-                        label="foto"                        
-                        >
-                        <Input placeholder="url artículo en renta." id="foto" />
+                        label="Ingrese los siguientes datos para dar de alta un artículo"                        
+                    >                        
                     </Form.Item>
 
                     <Form.Item
-                        label="artículo"                      
+                        label="Foto"                        
                         >
-                        <Input placeholder="nombre del artículo en renta. Ejemplo: Taladro Rotomartillo" id="articulo" />
+                        <Input placeholder="Url artículo en renta." id="Foto" />
                     </Form.Item>
 
+                    <Form.Item
+                        label="Artículo"                      
+                        >
+                        <Input placeholder="Nombre del artículo en renta. Ejemplo: Taladro Rotomartillo" id="Articulo" />
+                    </Form.Item>
+
+                    {/* <Form.Item 
+                        label="Descripción"                        
+                        >
+                        <Input placeholder="Breve descripción del articulo. Ejemplo: giro izq y der. 5 velocidades" id="Descripcion" />
+                    </Form.Item> */}
+
+                   
                     <Form.Item 
-                        label="descripción"                        
-                        >
-                        <Input placeholder="breve descripción del articulo. Ejemplo: giro izq y der. 5 velocidades" id="descripcion" />
+                        label="Descripción"                        
+                        > 
+                    <div style={{ margin: '12px 0' }} />
+                    <TextArea
+                        addonBefore="Descripción"
+                        value={value}
+                        onChange={this.onChange}
+                        placeholder="Breve descripción del articulo. Ejemplo: giro izq y der. 5 velocidades" id="Descripcion"
+                        autosize={{ minRows: 3, maxRows: 5 }}
+                    />
                     </Form.Item>
 
                     <Form.Item
-                        label="precio"                       
+                        label="Precio"                       
                         >
-                        <Input placeholder="costo de renta en pesos mexicanos del artículo por día. Ejemplo: $75.00" id="precio" />
+                        <Input placeholder="Costo de renta en pesos mexicanos del artículo por día. Ejemplo: $75.00" id="Precio" />
                     </Form.Item>
 
                     <Form.Item
-                         label="marca"                        
+                         label="Marca"                        
                          >
-                        <Input placeholder="marca del artículo. Ejemplo: craftsman" id="marca" />
+                        <Input placeholder="Marca del artículo. Ejemplo: craftsman" id="Marca" />
                     </Form.Item>                   
 
                     <Form.Item 
-                        label="status"                       
+                        label="Estatus"                       
                         >
-                        <Input placeholder="status del articulo. Ejemplo: disponible para renta ó actualmente en renta" id="status" />
-                        <Select defaultValue="1">
-                            <Option value="1">disponible para renta</Option>
-                            <Option value="2">actualmente en renta</Option>                            
+                        {/* <Input placeholder="Estatus del articulo. Ejemplo: disponible para renta ó actualmente en renta" id="Estatus" /> */}
+                        
+                        
+                        <Select defaultValue="">
+                            <Option value="">Seleccionar estatus</Option>                            
+                            <Option value="1">Disponible para renta</Option>
+                            <Option value="2">Actualmente en renta</Option>       
                         </Select>
                     </Form.Item>                    
 
-                    <Form.Item label="disponibilidad" style={{ marginBottom: 0 }}>
+                    <Form.Item label="Disponibilidad" style={{ marginBottom: 0 }}>
                         <Form.Item                            
-                            help="fecha de inicio"
+                            help="Fecha de inicio"
                             style={{ display: 'inline-block', width: 'calc(50% - 12px)' }}
                             >
                             <DatePicker />
                         </Form.Item>
                         <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}>-</span>
                         <Form.Item
-                            help="fecha final"
+                            help="Fecha final"
                             style={{ display: 'inline-block', width: 'calc(50% - 12px)' }}
                             >
                             <DatePicker />
                         </Form.Item>
-                    </Form.Item>                  
+                    </Form.Item>  
 
-                    <div>
+                    <Form.Item>                   
                         <Button 
                             style={{ display: 'inline-block', width: 'calc(50% - 12px)', marginleft: '100px'}}
                             // class="centrado"
@@ -101,11 +139,12 @@ export default class AddProduct extends Component {
                             //  style={{ marginLeft: "100px" }}  
                             onClick={this.toggleDatePicker}                             
                             >
-                            ver preliminar
+                            Ver preliminar
                         </Button>
+                    </Form.Item>
 
-                        <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}>-</span>
-                    
+                    <Form.Item>
+                        {/* <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}>-</span> */}
                         <Button                         
                             style={{ display: 'inline-block', width: 'calc(50% - 12px)' }}
                             // class="centrado"
@@ -113,10 +152,11 @@ export default class AddProduct extends Component {
                             size="small"
                             //  style={{ marginLeft: "100px" }}  
                             onClick={this.toggleDatePicker}                             
-                            >
-                            Guardar
+                        >
+                        Guardar
                         </Button>
-                    </div>    
+                    </Form.Item>
+                
                 </Form>               
 
             </div>
